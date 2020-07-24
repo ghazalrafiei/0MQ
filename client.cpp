5e10def1179f1cd28f4ad41e388a4b18ef73d6a1
ghazal rafiei <ghazal783@gmail.com>
2020-07-24 12:55:40 +0430

    Add spontanous login

--- client.cpp
+++ client.cpp
@@ -1,7 +1,7 @@
 #include <zmq.hpp>
 #include <ctime>
 #include <cassert>
-#include <cstdlib>        // random()  RAND_MAX
+#include <cstdlib> // random()  RAND_MAX
 #include <cstdio>
 #include <cstdarg>
 #include <csignal>
@@ -22,88 +22,103 @@ using json = nlohmann::json;
 std::vector<std::string> bridge;
 std::mutex mute;
 
-void client_tasks(std::string client_name){
+/// format:  {method = s_m, params = {message , to , from }
+/// format     {method = login, params = {myname}
+
+void client_tasks(std::string client_name )
+{
     zmq::context_t context(1);
-    zmq::socket_t client(context , ZMQ_DEALER);
+    zmq::socket_t client(context, ZMQ_DEALER);
 
     srandom((unsigned)time(NULL));
-    #if (defined (WIN32))
-        s_set_id(client, (intptr_t)args);
-    #else
-        s_set_id(client);          //Set a printable identity
-    #endif
+#if (defined(WIN32))
+    s_set_id(client, (intptr_t)args);
+#else
+    s_set_id(client); //Set a printable identity
+#endif
 
     client.connect("tcp://localhost:5672");
+
+    json login_json;
+    login_json["method"] = "login";
     
-    s_sendmore(client , "");
-    s_send(client , client_name);
-    
+    json params;
+    params["name"]=client_name;
+
+    login_json["params"] = params;
+
+    s_sendmore(client, "");
+    s_send(client, login_json.dump());
+
     zmq::pollitem_t item[]{
-        {client , 0 , ZMQ_POLLIN,0}
-    };
-    while(1){
-        
-        int rc = zmq::poll(item , 1 , 10);
-        if(rc>0){//receiving
-            if(item[0].revents & ZMQ_POLLIN){
-                s_recv(client); 
-                std::cout<<"received"<<std::endl;
-                std::string s ;
-                s = s_recv(client);
+        {client, 0, ZMQ_POLLIN, 0}};
+    while (1){
+        int rc = zmq::poll(item, 1, 15);
+        if (rc > 0){ //receiving json with format["to": , "from": , "message": ]
+            if (item[0].revents & ZMQ_POLLIN){
+                s_recv(client);
+                std::cout << "received" << std::endl;
+                std::string s = s_recv(client);
                 json j = json::parse(s);
-                std::string from = j["name"];
+                std::string from = j["from"];
                 std::string message = j["message"];
-                std::cout<<"*New Message from "<<from<<": "<<message<<std::endl;
+                std::cout << "*New Message from " << from << ": " << message << std::endl;
             }
         }
-        
-        else if(rc == 0 &&  !bridge.empty()){//sending
+
+        else if (rc == 0 && !bridge.empty()){ //sending
             mute.lock();
-            if(!bridge.empty()){
+            if (!bridge.empty())
+            {
                 std::string string_json_message = bridge.back();
                 bridge.pop_back();
-        
-                s_sendmore(client  ,"");
-                s_send(client , string_json_message);
+
+                s_sendmore(client, "");
+                s_send(client, string_json_message);
             }
             mute.unlock();
         }
-        
-    }   
+    }
 }
-void run(){
+void run(std::string my_name){
+    std::cout << "Who do you want to chat with?" << std::endl;
+    std::string rcvr_name;
+    std::cin >> rcvr_name;
+    std::cin.ignore();
+
     while (1){
-        std::cout<<"who do you want to message?"<<std::endl;
-        std::string receiver;
-        std::cin>>receiver;
-        
-        std::cin.ignore();
-        std::cout<<"type the message: "<<std::endl;
+        std::cout << "type the message: " << std::endl;
         std::string msg;
-        std::getline(std::cin , msg);
-        
+        std::getline(std::cin, msg);
+
         json json_message;
-        json_message["name"] = receiver;
-        json_message["message"] = msg;
+        json_message["method"] = "send_message";
+
+        json params;
+        params["from"] = my_name;
+        params["to"] = rcvr_name;
+        params["message"] = msg;
+
+        json_message["params"] = params;
         std::string js = json_message.dump();
-        
+
         mute.lock();
         bridge.push_back(js);
         mute.unlock();
     }
-    
 }
 
-int main(){
-    std::cout<<"Your name is: "<<std::endl;
+int main()
+{
+    std::cout << "Your name is: " << std::endl;
     std::string client_name;
-    std::cin>>client_name;
+    std::cin >> client_name;
+    std::cin.ignore();
+    
+    std::thread with_server(client_tasks, client_name);
+    std::thread with_user(run, client_name);
 
-    std::thread with_server(client_tasks,client_name);
-    std::thread with_user(run);
     with_server.join();
     with_user.join();
-    
-    
-    return 0;
 }
+
--- server.cpp
+++ server.cpp
@@ -1,8 +1,9 @@
 #include <pthread.h>
+#include <thread>
 #include <zmq.hpp>
 #include <ctime>
 #include <cassert>
-#include <cstdlib> // random()  RAND_MAX
+#include <cstdlib>        // random()  RAND_MAX
 #include <cstdio>
 #include <cstdarg>
 #include <csignal>
@@ -20,81 +21,71 @@
 
 using json = nlohmann::json;
 
-bool contain(std::vector<std::string> &v, std::string id)
-{
-    for (auto t : v)
-    {
-        if (t == id)
-        {
+std::unordered_map<std::string , std::string> id_to_name;
+
+bool contain(std::vector<std::string> &v, std::string id){
+    for(auto t : v){
+        if( t==id){
             return true;
         }
     }
     return false;
 }
 
-int main()
-{
+std::string name_to_id(std::string name){
+    for(auto it = id_to_name.begin(); it!= id_to_name.end(); ++it){
+            if((it->second) == name);
+            return (it->first);
+        }
+    return "";
+}
+
+int main(){
+    
+
     zmq::context_t context(1);
-    zmq::socket_t broker(context, ZMQ_ROUTER);
+    zmq::socket_t broker(context, ZMQ_ROUTER); 
 
     broker.bind("tcp://*:5672");
-    std::cout << "Connected" << std::endl;
+    std::cout<<"Connected"<<std::endl;
 
-    std::vector<std::string> id_list;
-    std::vector<std::string> name_list;
-    std::unordered_map<std::string, std::string> id_to_name;
-
-    std::cout << "How many clients?" << std::endl;
-    int number_of_clients;
-    std::cin >> number_of_clients;
-    std::cout << "Waiting for " << number_of_clients << " clients to connect..." << std::endl;
-
-    for (int i = 0; i < number_of_clients; ++i)
-    {
-        if (i > 0)
-            std::cout << "Waiting for the next client..." << std::endl;
+    while(1){
         std::string identity = s_recv(broker);
+        s_recv(broker);
+        std::string comming_message = s_recv(broker);
 
-        s_recv(broker);                    //Envelope delimiter
-        std::string name = s_recv(broker); //Say their name
+        json json_message = json::parse(comming_message);
 
-        id_list.push_back(identity);
-        name_list.push_back(name);
-        id_to_name[identity] = name;
-
-        std::cout << name << " connected" << std::endl;
-    }
-    std::cout << "All are connected" << std::endl;
-    //**Messaging**//
-    while (1)
-    {
-        std::cout << "Waiting for incoming message to send out... " << std::endl;
-
-        std::string sender_id = s_recv(broker);
-        s_recv(broker); //envelope delimiter
-        std::string string_json_message = s_recv(broker);
-
-        json msg = json::parse(string_json_message.c_str());
-        std::string rcvr_name = msg["name"];
-        std::string message = msg["message"];
-
-        std::string rcvr_id;
-        for (auto it = id_to_name.begin(); it != id_to_name.end(); ++it)
-        {
-            if (it->second == rcvr_name)
-                rcvr_id = it->first;
+        if(json_message["method"] == "send_message"){
+            json params = json_message["params"];
+            std::string rcvr_name = params["to"];
+            std::string rcvr_id = name_to_id(rcvr_name);
+    
+            if(rcvr_id != ""){
+                s_send(broker , rcvr_id);
+                std::cout<<id_to_name[identity]<<" to "<<rcvr_id<<" "<<name_to_id(rcvr_id);
+                s_send(broker , "");
+                s_send(broker , params.dump());
+                std::cout<<"A message sent to "<<id_to_name[rcvr_id]<<" "<<params<<std::endl;
+            }
+            else{
+                std::cout<<"receiver does not exist";
+            //reply to sender
+            }
         }
 
-        json j_to_send;
-        j_to_send["name"] = id_to_name[sender_id];
-        j_to_send["message"] = message;
+        else if(json_message["method"] == "login"){
+            std::string name = json_message["params"]["name"];
+            id_to_name[identity] = name;
+            std::cout<<name<<" connected "<<identity<<std::endl;
+        }
 
-        std::string s_to_send = j_to_send.dump();
 
-        s_sendmore(broker, rcvr_id);
-        s_sendmore(broker, "");
-        s_send(broker, s_to_send);
-        std::cout << "sent" << std::endl;
     }
+
+/// format:  {method = s_m, params = {message , to , from }
+/// format     {method = login, params = {myname}
+
+
     return 0;
 }
